e68acd5be8dd07c2927ae17bdb21ce66289f053d
M Nabeel Arshad <nabeel_arshad93@outlook.com>
2025-02-04 23:18:51 +0500

    guard added

--- app/Models/Superadmin.php
+++ app/Models/Superadmin.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Superadmin extends Authenticatable
 {
+    protected $guard = 'superadmin';
     protected $table = 'superadmins';
     protected $fillable = ['name', 'email', 'password'];
 }
